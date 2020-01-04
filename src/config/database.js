205925582f11f522205c98a40ae8815bafd1acbc
Admin <admin@mbp-rodrigocano.local>
2020-01-04 13:30:00 -0300

    Add sequelize inital config

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgress',
+  host: 'localhost',
+  username: 'postgress',
+  password: 'docker',
+  database: 'gympoint',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
