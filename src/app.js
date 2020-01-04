7563b97e56a38b2e9ff37490a317c7c0bba816c9
Admin <admin@mbp-rodrigocano.local>
2020-01-04 08:12:57 -0300

    Add eslint and prettier configs

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-import express from 'express'
-import routes from './routes'
+import express from 'express';
+import routes from './routes';
 
 class App {
   constructor() {
-    this.server = express()
+    this.server = express();
 
-    this.middlewares()
-    this.routes()
+    this.middlewares();
+    this.routes();
   }
 
   middlewares() {
@@ -18,4 +18,4 @@ class App {
   }
 }
 
-export default new App().server
+export default new App().server;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,7 @@
-import { Router } from 'express'
+import { Router } from 'express';
 
-const routes = new Router()
+const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'deu ruim' })
-})
+routes.get('/', (req, res) => res.json({ message: 'deu bom' }));
 
-export default routes
+export default routes;
